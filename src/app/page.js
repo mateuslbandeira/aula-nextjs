d84a28537e8a1584202a8de773da1adac558173b
MATEUS LOPES BANDEIRA <mateus.bandeira>
2026-02-27 13:48:26 -0300

    aula css

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,13 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-const Card = () => {
-  return (
-    <div>
-      <h3>Aula introdução</h3>
-      <h3>Aula Estilização</h3>
-      <h3>Aula Componentes</h3>
-    </div>
-  );
-}
-
 function Home() {
   return (
     <div>
       <h1>React com Next JS</h1>
+      <h2>Aula de introdução</h2>
+      <h3>Leiaute e Estilização</h3>
+      <p>Nesta aula iremos entender o uso de CSS global e local.</p>
     </div>
   );
 }
