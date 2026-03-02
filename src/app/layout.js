35f255d29a0f1e715498a14ed527a36101ba1f8e
MATEUS LOPES BANDEIRA <mateus.bandeira>
2026-03-02 15:25:08 -0300

    Aula CSS

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
-import "./estilizacao/globals.css";
 import "./estilizacao/reset.css";
+import "./estilizacao/globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
