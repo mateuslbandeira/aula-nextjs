5b429c6439ea69ed7b318d3aa279934255db842a
MATEUS LOPES BANDEIRA <mateus.bandeira>
2026-03-16 15:24:47 -0300

    Atividade 1

--- src/app/atividades/atv-01/page.jsx
+++ src/app/atividades/atv-01/page.jsx
@@ -1,8 +1,10 @@
+import style from "./page.module.css";
 function Atividade01 () {
     return (
         <div>
-            <h1>Atividade 1</h1>
-            <p>Componente básico para renderizar tela com React</p>
+            <h1 className={style.containerTitulo}>Atividade 1</h1>
+            <h2 className={style.containerTexto}>A poluição é um grande problema no mundo 🌍. Ela acontece quando o ar, a água e o solo são contaminados por lixo, fumaça e produtos químicos. A poluição prejudica a natureza, os animais e a saúde das pessoas. Por isso, é importante cuidar do meio ambiente e evitar o desperdício. </h2>
+            
         </div>
     )
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -12,7 +12,7 @@ function Home() {
         </div>
         <div className={styles.containerListas}>
             <h1>Atividades</h1>
-            <Link href="/atividade/atv-01">Atividade 1</Link>
+            <Link href="/atividades/atv-01">Atividade 1</Link>
         </div>
         </div>
     );
